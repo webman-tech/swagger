3f18f30d48b11a4a40ba80b5c37423ddc95eea54
kriss <462679766@qq.com>
2025-07-10 19:23:52 +0800

    feature: swagger 静态资源支持配置基础 url

--- src/DTO/ConfigSwaggerUiDTO.php
+++ src/DTO/ConfigSwaggerUiDTO.php
@@ -11,21 +11,23 @@ final class ConfigSwaggerUiDTO extends BaseConfigDTO
     public function __construct(
         public string  $view = 'swagger-ui', // 视图名称
         public ?string $view_path = null, // 视图路径，相对 app_path() 的路径
+        public string  $assets_base_url = 'https://unpkg.com/swagger-ui-dist',
         public array   $data = [], // 视图数据
     )
     {
         $this->view_path ??= ConfigHelper::getViewPath();
         $appName = (string)config('app.name', 'swagger');
+        $this->assets_base_url = rtrim($this->assets_base_url, '/');
         $this->data = ArrayHelper::merge(
             [
                 // @link https://github.com/swagger-api/swagger-ui/blob/master/dist/swagger-initializer.js
                 'css' => [
-                    'https://unpkg.com/swagger-ui-dist/swagger-ui.css',
-                    //'https://unpkg.com/swagger-ui-dist/index.css',
+                    '/swagger-ui.css', // 修改 cdn 地址
+                    //'index.css',
                 ],
                 'js' => [
-                    'https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js',
-                    //'https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js',
+                    '/swagger-ui-bundle.js',
+                    //'swagger-ui-standalone-preset.js',
                 ],
                 'title' => $appName . ' - openapi',
                 'ui_config' => [
@@ -46,10 +48,22 @@ final class ConfigSwaggerUiDTO extends BaseConfigDTO
             ],
             $this->data,
         );
+        $this->data['css'] = $this->processAssetUrl($this->data['css']);
+        $this->data['js'] = $this->processAssetUrl($this->data['js']);
     }
 
     protected static function getAppConfig(): array
     {
         return ConfigHelper::get('app.swagger_ui', []);
+    }
+
+    private function processAssetUrl(array $data): array
+    {
+        return array_map(function (string $item) {
+            if (!str_starts_with($item, 'https://') && !str_starts_with($item, 'http://')) {
+                $item = $this->assets_base_url . '/' . ltrim($item, '/');
+            }
+            return $item;
+        }, $data);
     }
 }

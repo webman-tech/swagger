8b379d1982ef9be3b9378869d3be96f17f042ef8
kriss <462679766@qq.com>
2025-07-13 19:47:36 -0600

    feature: 对 path 进行排序

--- src/RouteAnnotation/Processors/SortComponentsProcessor.php
+++ src/RouteAnnotation/Processors/SortComponentsProcessor.php
@@ -3,6 +3,8 @@
 namespace WebmanTech\Swagger\RouteAnnotation\Processors;
 
 use OpenApi\Analysis;
+use OpenApi\Annotations\PathItem;
+use OpenApi\Annotations\Schema;
 
 /**
  * 对组件排序，防止扫描顺序不一致，导致多次扫描输出结果顺序不一致的情况
@@ -13,8 +15,13 @@ final class SortComponentsProcessor
     public function __invoke(Analysis $analysis)
     {
         if (is_object($analysis->openapi->components) && is_iterable($analysis->openapi->components->schemas)) {
-            usort($analysis->openapi->components->schemas, function ($a, $b) {
+            usort($analysis->openapi->components->schemas, function (Schema $a, Schema $b) {
                 return strcmp($a->schema, $b->schema);
+            });
+        }
+        if (is_iterable($analysis->openapi->paths)) {
+            usort($analysis->openapi->paths, function (PathItem $a, PathItem $b) {
+                return strcmp($a->path, $b->path);
             });
         }
     }
